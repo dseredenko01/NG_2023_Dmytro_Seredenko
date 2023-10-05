022dbcbb8473b623a632f62f047f9d3b511bb60e
Dmytro Seredenko <dimkaser2001@gmail.com>
2023-10-05 20:50:58 +0300

    Updated first task of first lection.

--- lection_1/task_1/main.py
+++ lection_1/task_1/main.py
@@ -1,6 +1,2 @@
-
-name = input("Enter your first name: ")
-surname = input("Enter your last name: ")
-
-print(f"Your first name is {name}")
-print(f"Your last name is {surname}")
+print(f"Your first name is John")
+print(f"Your last name is Smith")
