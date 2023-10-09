2c40625905bef57c9cfe3630ae8e4f6522599a08
Dmytro Seredenko <dimkaser2001@gmail.com>
2023-10-09 13:53:50 +0300

    Updated task 2.

--- lection_1/task_2/main.py
+++ lection_1/task_2/main.py
@@ -1,6 +1,8 @@
-x = float(input("Please, enter a first random number: "))
-y = float(input("Please, enter a second random number: "))
+input("Hello, do you want to find the sum of two numbers? (Yes/No): ")
 
-c = x + y
+x = float(input("Please, enter the first  number: "))
+y = float(input("Please, enter the second  number: "))
+
+sum = x + y
 
 print(f"The sum of two numbers is: {c}")
