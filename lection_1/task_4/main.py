102caa69ce08ac8a36be11ca948a4414466acc22
Dmytro Seredenko <dimkaser2001@gmail.com>
2023-10-24 21:19:12 +0300

    Fix copypaste and added updated task

--- lection_1/task_4/main.py
+++ lection_1/task_4/main.py
@@ -5,25 +5,17 @@ operator = (input("Enter an operator (+ - * / √ x^2): "))
 num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 
-#num1 = float(input("Enter the 1st number: "))
-#num2 = float(input("Enter the 2nd number: "))
-
 match operator:
     case '+':
-        result = num1 + num2
-        print(round(result, 4))
+        print(round(num1 + num2, 4))
     case '-':
-        result = num1 - num2
-        print(round(result, 4))
+        print(round(num1 - num2, 4))
     case '*':
-        result = num1 * num2
-        print(round(result, 4))
+        print(round(num1 * num2, 4))
     case '/':
-        result = num1 / num2
-        print(round(result, 4))
+        print(round(num1 / num2, 4))
     case '√':
-        result = math.sqrt(num1)
-        print(round(result, 4))
+        print(round(math.sqrt(num1), 4))
     case 'x^2':
         result = pow(num1, 2)
         print(round(result, 4))
